d614edbc46be05266770023f2a953d5a9ee87105
Jestoni Obice <obicerjestoni@gmail.com>
2023-05-02 16:04:53 -0400

    Add bookmarks heading

--- src/app/components/Links.tsx
+++ src/app/components/Links.tsx
@@ -14,11 +14,16 @@ type Props = {
 
 const Links: React.FC<Props> = ({ links, deleteLink }) => {
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-flow-row auto-rows-max gap-2 mt-8 mb-2 w-full md:w-3/4 mx-auto">
-      {links.slice(0).reverse().map((link, index) => (
-        <LinkCard key={index} link={link} deleteLink={deleteLink} />
-      ))}
-    </div>
+    <>
+      <div className='w-full md:w-3/4 mt-12'>
+        <h2 className='text-xl'>Bookmarks</h2>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-flow-row auto-rows-max gap-2 mb-2 mt-4 w-full md:w-3/4 mx-auto">
+        {links.slice(0).reverse().map((link, index) => (
+          <LinkCard key={index} link={link} deleteLink={deleteLink} />
+        ))}
+      </div>
+    </>
   )
 }
 
